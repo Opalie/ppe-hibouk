47372ee55405af645b049617de255ab984db187c
KadenHD <71968563+KadenHD@users.noreply.github.com>
2020-12-26 17:24:53 +0100

    Add files via upload

---
+++ Website/copyright.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="utf-8">
+    <title>copyright</title>
+    <link rel="stylesheet" href="style/copyright.css">
+</head>
+
+<footer>
+        <div class="footerbottom">
+            <p>Copyright &copy; Vélès Corporation - 2020. All rights reserved.</p>
+        </div>
+</footer>
