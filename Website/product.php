39ce883f6704e2ff231f8194d5cbc4f9d795b108
Opalie <31365045+Opalie@users.noreply.github.com>
2021-01-04 12:14:25 +0100

    Update product.php
    oops pushed the wrong file

--- Website/product.php
+++ Website/product.php
@@ -1,20 +1,21 @@
 <?php
 
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=ppe;charset=UTF8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=ppe;charset=UTF8', 'root', 'Xk3QPGYBBeEiRvZI');
   } catch (Exception $e) {
     die('Erreur :' . $e->getMessage());
   }
 
 
 // Check to make sure the id parameter is specified in the URL
-if (isset($_GET['ebook_id'])) {
+if (isset($_GET['book_id'])) {
     // Prepare statement and execute, prevents SQL injection
-    $stmt = $bdd->prepare('SELECT * FROM ebook WHERE ebook_id = ?');
-    $stmt->execute([$_GET['ebook_id']]);
+    $stmt = $bdd->prepare('SELECT * FROM ebook WHERE book_id = ?');
+    $stmt->execute([$_GET['book_id']]);
     // Fetch the Ebook from the database and return the result as an Array
     $ebook = $stmt->fetch(PDO::FETCH_ASSOC);
     // Check if the Ebook exists (array is not empty)
+
     if (!$ebook) {
         // Simple error to display if the id for the Ebook doesn't exists (array is empty)
         exit('Ebook does not exist!');
@@ -32,7 +33,7 @@ if (isset($_GET['ebook_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $ebook['ebook_name'] ?> - <?= $ebook['ebook_author'] ?></title>
+    <title><?= $ebook['book_title'] ?> - <?= $ebook['book_author'] ?></title>
 </head>
 
 <body>
@@ -40,20 +41,20 @@ if (isset($_GET['ebook_id'])) {
     <div class="page">
 
         <div class="left">
-            <img src="imgs/<?= $ebook['ebook_cover'] ?>" alt="<?= $ebook['ebook_name'] ?>">
+            <img src="<?= $ebook['book_cover'] ?>" alt="<?= $ebook['book_title'] ?>">
             <span class="price">
-                &euro;<?= $ebook['ebook_price'] ?>
+                &euro;<?= $ebook['book_price'] ?>
             </span>
             <button type="submit">Add to cart</button>
         </div>
 
         <div class="right">
-            <h1 class="name"><?= $ebook['ebook_name'] ?> - <?= $ebook['ebook_author'] ?> (<?= $ebook['ebook_date'] ?>)</h1>
+            <h1 class="name"><?= $ebook['book_title'] ?> - <?= $ebook['book_author'] ?> (<?= $ebook['book_date'] ?>)</h1>
             <hr>
-            <?= $ebook['ebook_genre'] ?>
+            <?= $ebook['book_genre'] ?>
             <br>
             <div class="description">
-                <?= $ebook['ebook_desc'] ?>
+                <?= $ebook['book_desc'] ?>
             </div>
         </div>
 
