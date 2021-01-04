92eeaafde45e03be0daba538c10d7b17291512d4
Fury6z <siyouzik@gmail.com>
2021-01-04 15:08:59 +0100

    Test before merge

--- Website/session_function.php
+++ Website/session_function.php
@@ -1,8 +1,19 @@
 <!-------- Redirige sur le login si non connecté -------->
 <?php
 session_start();
-if(!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id'])) {
     header('location:connect.php');
 }
 ?>
 <!------------------------------------------------------->
+
+<!------------------------------- Connecte à la bdd  --------------------------------------------->
+<?php
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=ppe;charset=UTF8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur :' . $e->getMessage());
+}
+?>
+<!------------------------------------------------------------------------------------------------>
+
