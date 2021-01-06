97d1aec039a741ffe5dc999b6e10c1f3f271784c
Opalie <31365045+Opalie@users.noreply.github.com>
2021-01-06 16:03:36 +0100

    db settings with no password

--- Website/include/_dbSettings.php
+++ Website/include/_dbSettings.php
@@ -1,7 +1,7 @@
 <!------------------------------- Connecte à la bdd  --------------------------------------------->
 <?php
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=ppe;charset=UTF8', 'root', 'Xk3QPGYBBeEiRvZI');
+    $bdd = new PDO('mysql:host=localhost;dbname=ppe;charset=UTF8', 'root', '');
 } catch (Exception $e) {
     die('Erreur :' . $e->getMessage());
 }
