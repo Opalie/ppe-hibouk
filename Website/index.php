4900649f1e20f074239c2ea618345460e7d8b4d0
elyaDev <siyouzik@gmail.com>
2021-01-06 16:41:29 +0100

    Fix css error

--- Website/index.php
+++ Website/index.php
@@ -9,6 +9,9 @@ include_once './include/session_function.php';
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <!------ Include the above in your HEAD tag ---------->
+
     <script src="https://kit.fontawesome.com/64d58efce2.js"></script>
     <link rel="stylesheet" href="style/index.css" />
     <title>Profile</title>
@@ -20,7 +23,7 @@ include_once './include/navbar.php';
 
 <body>
 
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
             <div class="col-md-3 col-sm-6">
                 <div class="product-grid2">
@@ -37,8 +40,8 @@ include_once './include/navbar.php';
                         <a class="add-to-cart" href="">Add to cart</a>
                     </div>
                     <div class="product-content">
-                        <h3 class="title"><a href="#">Women's Designer Top</a></h3>
-                        <span class="price">$599.99</span>
+                        <h3 class="title"><a href="#">ebook</a></h3>
+                        <span class="price">11,00€</span>
                     </div>
                 </div>
             </div>
@@ -57,8 +60,8 @@ include_once './include/navbar.php';
                         <a class="add-to-cart" href="">Add to cart</a>
                     </div>
                     <div class="product-content">
-                        <h3 class="title"><a href="#">Women's Yellow Top</a></h3>
-                        <span class="price">$699.99</span>
+                        <h3 class="title"><a href="#">ebook</a></h3>
+                        <span class="price">11,00€</span>
                     </div>
                 </div>
             </div>
@@ -77,8 +80,8 @@ include_once './include/navbar.php';
                         <a class="add-to-cart" href="">Add to cart</a>
                     </div>
                     <div class="product-content">
-                        <h3 class="title"><a href="#">Women's Designer Top</a></h3>
-                        <span class="price">$599.99</span>
+                        <h3 class="title"><a href="#">ebook</a></h3>
+                        <span class="price">11,00€</span>
                     </div>
                 </div>
             </div>
@@ -97,8 +100,8 @@ include_once './include/navbar.php';
                         <a class="add-to-cart" href="">Add to cart</a>
                     </div>
                     <div class="product-content">
-                        <h3 class="title"><a href="#">Women's Designer Top</a></h3>
-                        <span class="price">$599.99</span>
+                        <h3 class="title"><a href="#">ebook</a></h3>
+                        <span class="price">11,00€</span>
                     </div>
                 </div>
             </div>
@@ -113,5 +116,8 @@ include_once './include/copyright.php';
 ?>
 
 <script src="js/.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 
 </html>
